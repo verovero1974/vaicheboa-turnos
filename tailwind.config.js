e7f430e46e582d4379108779d0a23c824fea5239
verovero1974 <veronicadelgadoneira@gmail.com>
2025-06-03 17:00:14 +0200

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,16 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ['Inter', 'sans-serif'],
+      colors: {
+        'vaiche-azul': '#009FE3',
+        'vaiche-azulOscuro': '#0077b6',
+        'vaiche-amarillo': '#F4A300',
+        'vaiche-acento': '#f0f9ff'
       },
-    },
+      fontFamily: {
+        vaiche: ['Fredoka', 'sans-serif']
+      }
+    }
   },
-  plugins: [],
-};
+  plugins: []
+}
