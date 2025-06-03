41c5ad1eabdfcb962a457a9e753cd27ccdd34807
verovero1974 <veronicadelgadoneira@gmail.com>
2025-06-03 17:03:17 +0200

    Updated VaicheboaCard.tsx

--- src/VaicheboaCard.tsx
+++ src/VaicheboaCard.tsx
@@ -1,7 +1,7 @@
 export default function VaicheCard() {
   return (
     <div className="bg-vaiche-azul text-white font-vaiche max-w-md mx-auto mt-10 p-6 rounded-xl shadow-lg">
-      <h2 className="text-2xl font-bold mb-2">Bienvenida a Vaiche Boa</h2>
+      <h2 className="text-2xl font-bold mb-2">Bienvenido a Vaiche Boa</h2>
       <p className="text-vaiche-amarillo mb-4">
         "Cenamos el viernes pasado, buen trato del personal y comida fantástica."
       </p>
